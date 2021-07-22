c6c8bab68d5dff069d1903907569d4d250df7495
Anar Agaev <anar.n.agaev@gmail.com>
2021-07-22 12:04:33 +0300

    Removed callback modal into include arrea

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -60,25 +60,12 @@
         </div>
 
 
+
+
+
+
         <!-- Добавлять мадальные сообщения только на страницах с соответствующими формами
-        <div class="modal" id="calbackModal" data-source-reset="#callbackForm">
-            <div class="modal__dialog">
-                <div class="modal__content">
-                    <div class="modal__header">
-                        <div class="modal__title">Заявка отправлена</div>
-                        <div class="modal__subtitle">Перезвоним вам в ближайшее время</div>
-                    </div>
-                    <div class="modal__body">
-                        <img class="modal-img" src="/img/icon-modal-phone.svg" alt="">
-                    </div>
-                    <div class="modal__footer">
-                        <button class="btn btn_primary not-focused modal__close">
-                            Закрыть окно
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
+
         <div class="modal" id="orderModal" data-source-reset="#orderForm">
             <div class="modal__dialog">
                 <div class="modal__content">
@@ -154,6 +141,20 @@
 
 
 
+        <?
+            if ($APPLICATION->GetCurPage() == '/') {
+                $APPLICATION->IncludeComponent(
+                    "bitrix:main.include",
+                    "",
+                    Array(
+                        "AREA_FILE_SHOW" => "file",
+                        "AREA_FILE_SUFFIX" => "inc",
+                        "EDIT_TEMPLATE" => "",
+                        "PATH" => "/include/callback-modal.php"
+                    )
+                );
+            }
+        ?>
         <?
             if ($APPLICATION->GetCurPage() == '/') {
                 $APPLICATION->IncludeComponent(
