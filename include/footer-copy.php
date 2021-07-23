91850cfb33721a41edb8f0530609e26aaf63f326
Anar Agaev <anar.n.agaev@gmail.com>
2021-07-23 15:32:51 +0300

    Deleted simple version

--- include/footer-copy.php
+++
@@ -1,2 +0,0 @@
-Дизайн - <a class="revers" href="https://www.behance.net/maltsevoleg" target="_blank">
-Олег Мальцев</a>
--- local/templates/main/description.php
+++
@@ -1,9 +0,0 @@
-<?
-if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
-	die();
-
-$arTemplate = Array(
-	'NAME' => 'Базовый шаблон сайта Ekranika',
-	'DESCRIPTION' => 'Шаблон сайта по умолчанию. Используется на всех стандартных страницах сайта.'
-);
-?>
