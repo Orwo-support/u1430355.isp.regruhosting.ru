78e954aee5f5f0538bdf3a371818b13fa3396f2b
Anar Agaev <anar.n.agaev@gmail.com>
2021-07-24 16:12:36 +0300

    Fix click on header dorp links

--- layout/src/pug/templates/header/header.js
+++ layout/src/pug/templates/header/header.js
@@ -24,26 +24,20 @@ $(document).ready(function () {
     $('#navToggler').on('click', function () {
         $('#header').toggleClass('nav-visible hover-with-quiz');
         $('.nav__item.dropdown').removeClass('drop-visible');
-        $('.nav__item.dropdown > a').removeClass('drop-visible');
     });
 
-    $('.nav__item.dropdown').on('click', function (e) {
-        const el = e.target
+    $('.nav__item.dropdown').on(
+        'click',
+        e => {
+            let el = $(e.target).hasClass('dropdown')
+                ? e.target
+                : $(e.target).parent('.dropdown')[0];
 
-        if (!isLaptop) {
-            $(el).toggleClass('drop-visible');
+            if (el && (getScreenType() === 'md' || getScreenType() === 'sm')) {
+                $(el).toggleClass('drop-visible');
+            }
         }
-    });
-
-    $('.nav__item.dropdown a').on('click', function (e) {
-        if (!isLaptop) {
-            e.preventDefault();
-
-            const el = e.target;
-            const parent = $(el).parent();
-            parent.click();
-        }
-    });
+    );
 
     $('.nav__points-btn').on('click', function (e) {
         e.preventDefault();
