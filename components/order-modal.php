35500b9405eefedac551f26d551e543fd13981d8
Anar Agaev <anar.n.agaev@gmail.com>
2021-07-30 12:37:46 +0300

    Mdifyied callback and collback form, ofrder and video modals and quiz

---
+++ components/order-modal.php
@@ -0,0 +1,22 @@
+<div class="modal" id="orderModal" data-source-reset="#orderForm">
+    <div class="modal__dialog">
+        <div class="modal__content">
+            <div class="modal__header">
+                <div class="modal__title">
+                    Заявка отправлена
+                </div>
+                <div class="modal__subtitle">
+                    Перезвоним вам в ближайшее время
+                </div>
+            </div>
+            <div class="modal__body">
+                <img class="modal-img pic" src="/img/icon-modal-phone.svg" alt="">
+            </div>
+            <div class="modal__footer">
+                <button class="btn btn_primary not-focused modal__close">
+                    Закрыть окно
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
