99c686a2d249b6e15a012554bfdca36425c0b636
Anar.N.Agaev - Ekranica developer <anar.n.agaev@gmail.com>
2021-08-09 09:19:15 +0300

    Reduced the running time of the script from 7.00 to 19.00

--- utilities/exchange-rate.php
+++ utilities/exchange-rate.php
@@ -53,91 +53,96 @@
     
   } else {
     
-    // Дата последнего обновления курса в БД
-    $dateDBUpdate = date('d.m.Y', strtotime($ARR_CURRENT_EXCHANGE_RATE['dateEntryCreate']));
-    
-    // Текущая дата
-    $currentDate = date('d.m.Y');
-
-    // Если дата обновления курса в БД не равна текущей 
-    // дате (т.е. сегодня курс ещё не обновляли),
-    // делаем запрос курса на сайт ЦБР
-    if (strtotime($dateDBUpdate) != strtotime($currentDate))  {
+    // Что бы не делать лишних запросов к серверу ЦБР, 
+    // заправшиваем даанные только с 7.00 утра до 19.00 вечера по МСК
+    if (intval(date('H')) >= 7 and intval(date('H')) <= 19) {
       
-      $file       = file_get_contents("https://www.cbr.ru/scripts/XML_daily.asp");
-      $xml        = simplexml_load_string($file);
-      $arr        = json_decode(json_encode($xml), true);
-      $arrUsd     = array_values(array_filter($arr["Valute"], "filterValutes"))["0"];
-
-      $market     = $arr["@attributes"]["name"];
-      $dateUpdate = $arr["@attributes"]["Date"];
-
-      $valuteId   = $arrUsd["@attributes"]["ID"];
-      $numCode    = $arrUsd["NumCode"];
-      $charCode   = $arrUsd["CharCode"];
-      $nominal    = $arrUsd["Nominal"];
-      $name       = $arrUsd["Name"];
-      $value      = $arrUsd["Value"];
+      // Дата последнего обновления курса в БД
+      $dateDBUpdate = date('d.m.Y', strtotime($ARR_CURRENT_EXCHANGE_RATE['dateEntryCreate']));
       
-      //print '<pre>';
-      //print 'Рынок: ' . $market . '<br>';
-      //print 'Дата обновления курса: ' . $dateUpdate . '<br>';
-      //print 'Идентификатор валюты: ' . $valuteId . '<br>';
-      //print 'Цифровой код валюты: ' . $numCode . '<br>';
-      //print 'Символьный код валюты: ' . $charCode . '<br>';
-      //print 'Номинал: ' . $nominal . '<br>';
-      //print 'Наименование: ' . $name . '<br>';
-      //print 'Значение: ' . $value . '<br>';
-      //print 'JSON XML ответа ЦБР: ' . $JsonXmlRequest;
-      
-        
-      // Если дата обновления курса из запроса к ЦБР
-      // равна текущей (т.е. это курс за сегодня)
-      // добавляем новый курс в БД
-      if (strtotime($dateUpdate) == strtotime($currentDate)) {
-        
-        CModule::IncludeModule('iblock');
-        
-        $el = new CIBlockElement; // создаём свой класс
-        $iblock_id = 72; // ID инфоблока в который добавляем новый элемент
-        
-        
-        // Имена свойств в массиве $PROPS должны соответствовать
-        // свойствами инфоблока в который будет добавлен новый элемент
-        $PROPS['MARKET'] = $market;
-        $PROPS['DATE_UPDATE'] = $dateUpdate;
-        $PROPS['VALUTE_ID'] = $valuteId;
-        $PROPS['NUM_CODE'] = $numCode;
-        $PROPS['CHAR_CODE'] = $charCode;
-        $PROPS['NOMINAL'] = $nominal;
-        $PROPS['NAME'] = $name;
-        $PROPS['VALUE'] = $value;
-        $PROPS['JSON_XML_REQUEST'] = defined('JSON_UNESCAPED_UNICODE')
-          ? json_encode($arr, JSON_UNESCAPED_UNICODE)
-          : json_encode($arr);     
+      // Текущая дата
+      $currentDate = date('d.m.Y');
 
+      // Если дата обновления курса в БД не равна текущей 
+      // дате (т.е. сегодня курс ещё не обновляли),
+      // делаем запрос курса на сайт ЦБР
+      if (strtotime($dateDBUpdate) != strtotime($currentDate))  {
         
-        // Основные поля добавлямого в инфоблок элемента
-        $fields = array(
-            "DATE_CREATE" => date("d.m.Y H:i:s"), // Передаем дату создания
-            "CREATED_BY" => $GLOBALS['USER']->GetID(), // Передаем ID пользователя добавившего сообщение
-            "IBLOCK_SECTION_ID" => false, // ID разделов. В нашем случае false, т.к. нет подразделов
-            "IBLOCK_ID" => $iblock_id, // ID информационного блока он 72-ый в нашем случае
-            "PROPERTY_VALUES" => $PROPS, // Передаем массив значении для свойств
-            "NAME" => "Ekranika", // Не может быть пустой строкой
-            "ACTIVE" => "Y", // поумолчанию делаем активным или ставим N для отключении поумолчанию
-            "PREVIEW_TEXT" => "", // текс для анонса
-            "PREVIEW_PICTURE" => "", // изображение для анонса
-            "DETAIL_TEXT"    => "", // текст для детальной страницы
-            "DETAIL_PICTURE" => "" // изображение для детальной страницы
-        );
+        $file       = file_get_contents("https://www.cbr.ru/scripts/XML_daily.asp");
+        $xml        = simplexml_load_string($file);
+        $arr        = json_decode(json_encode($xml), true);
+        $arrUsd     = array_values(array_filter($arr["Valute"], "filterValutes"))["0"];
+
+        $market     = $arr["@attributes"]["name"];
+        $dateUpdate = $arr["@attributes"]["Date"];
+
+        $valuteId   = $arrUsd["@attributes"]["ID"];
+        $numCode    = $arrUsd["NumCode"];
+        $charCode   = $arrUsd["CharCode"];
+        $nominal    = $arrUsd["Nominal"];
+        $name       = $arrUsd["Name"];
+        $value      = $arrUsd["Value"];
         
+        //print '<pre>';
+        //print 'Рынок: ' . $market . '<br>';
+        //print 'Дата обновления курса: ' . $dateUpdate . '<br>';
+        //print 'Идентификатор валюты: ' . $valuteId . '<br>';
+        //print 'Цифровой код валюты: ' . $numCode . '<br>';
+        //print 'Символьный код валюты: ' . $charCode . '<br>';
+        //print 'Номинал: ' . $nominal . '<br>';
+        //print 'Наименование: ' . $name . '<br>';
+        //print 'Значение: ' . $value . '<br>';
+        //print 'JSON XML ответа ЦБР: ' . $JsonXmlRequest;
         
-        if ($ID = $el->Add($fields)) {
-            // echo $ID;
-        } else {
-            // В случае ошибки добавления нового курса доллара в БД, 
-            // можно отправлять письмо на почту модератеру.
+          
+        // Если дата обновления курса из запроса к ЦБР
+        // равна текущей (т.е. это курс за сегодня)
+        // добавляем новый курс в БД
+        if (strtotime($dateUpdate) == strtotime($currentDate)) {
+          
+          CModule::IncludeModule('iblock');
+          
+          $el = new CIBlockElement; // создаём свой класс
+          $iblock_id = 72; // ID инфоблока в который добавляем новый элемент
+          
+          
+          // Имена свойств в массиве $PROPS должны соответствовать
+          // свойствами инфоблока в который будет добавлен новый элемент
+          $PROPS['MARKET'] = $market;
+          $PROPS['DATE_UPDATE'] = $dateUpdate;
+          $PROPS['VALUTE_ID'] = $valuteId;
+          $PROPS['NUM_CODE'] = $numCode;
+          $PROPS['CHAR_CODE'] = $charCode;
+          $PROPS['NOMINAL'] = $nominal;
+          $PROPS['NAME'] = $name;
+          $PROPS['VALUE'] = $value;
+          $PROPS['JSON_XML_REQUEST'] = defined('JSON_UNESCAPED_UNICODE')
+            ? json_encode($arr, JSON_UNESCAPED_UNICODE)
+            : json_encode($arr);     
+
+          
+          // Основные поля добавлямого в инфоблок элемента
+          $fields = array(
+              "DATE_CREATE" => date("d.m.Y H:i:s"), // Передаем дату создания
+              "CREATED_BY" => $GLOBALS['USER']->GetID(), // Передаем ID пользователя добавившего сообщение
+              "IBLOCK_SECTION_ID" => false, // ID разделов. В нашем случае false, т.к. нет подразделов
+              "IBLOCK_ID" => $iblock_id, // ID информационного блока он 72-ый в нашем случае
+              "PROPERTY_VALUES" => $PROPS, // Передаем массив значении для свойств
+              "NAME" => "Ekranika", // Не может быть пустой строкой
+              "ACTIVE" => "Y", // поумолчанию делаем активным или ставим N для отключении поумолчанию
+              "PREVIEW_TEXT" => "", // текс для анонса
+              "PREVIEW_PICTURE" => "", // изображение для анонса
+              "DETAIL_TEXT"    => "", // текст для детальной страницы
+              "DETAIL_PICTURE" => "" // изображение для детальной страницы
+          );
+          
+          
+          if ($ID = $el->Add($fields)) {
+              // echo $ID;
+          } else {
+              // В случае ошибки добавления нового курса доллара в БД, 
+              // можно отправлять письмо на почту модератеру.
+          }
         }
       }
     }
