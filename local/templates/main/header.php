53de6768d78779ddd17f910207bbb8f240e3f388
Anar Agaev <anar.n.agaev@gmail.com>
2021-08-16 17:05:40 +0300

    Fixed width of text for banner sliders

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -35,11 +35,11 @@
             Asset::getInstance()->addString('<meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png">');
             Asset::getInstance()->addString('<meta name="theme-color" content="#ffffff">');
 
-            Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/css/vendors.02bc7b0d716e6b66ab4b.css");
-            Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/css/main.02bc7b0d716e6b66ab4b.css");
+            Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/css/vendors.c968c1e4d619d79118fd.css");
+            Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/css/main.c968c1e4d619d79118fd.css");
 
-            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/vendors.02bc7b0d716e6b66ab4b.js");
-            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/main.02bc7b0d716e6b66ab4b.js");
+            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/vendors.c968c1e4d619d79118fd.js");
+            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/main.c968c1e4d619d79118fd.js");
 
             if ($APPLICATION->GetCurPage() == '/o-nas-garantiya-kontakty/') {
                 Asset::getInstance()->addJs('https://api-maps.yandex.ru/2.1/?apikey=4c74d479-972b-4c76-81e7-b1bc63268173&amp;lang=ru_RU');
